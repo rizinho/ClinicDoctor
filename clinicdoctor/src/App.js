040dbe6de0d9f07482c51ed2484c0e8fac806dda
rizinho <135022344+rizinho@users.noreply.github.com>
2024-03-21 11:46:20 -0300

    bteste2

--- clinicdoctor/src/App.js
+++ clinicdoctor/src/App.js
@@ -1,18 +1,38 @@
 import styles from './App.module.css'
 import React from 'react'
 
+/*imagens*/
+import icon from './assets/logotcc.png'
+import img1 from './assets/img1.jpg'
+/*Fim imagens*/
+
 const App = () => {
   return ( 
 
     <div className={styles.main}>
         <div className={styles.header}>
 
-          <button className={styles.bDuvidas}>Duvidas</button>
+          <img  className={styles.icon} src={icon} alt='Imagem do logo'/>
+
+          <h1 className={styles.txtLog}>Consult Doctor</h1>
+
+          <button className={styles.bDuvidas}>Dúvidas</button>
           <button className={styles.bSac}>SAC</button>
           <button className={styles.bConfig}>Configurações</button>
           <button className={styles.bLogin}>Login</button>
 
         </div>
+
+        {/*Body*/}
+
+       <div className={styles.container}>
+
+         <img className={styles.imgContainer} src={img1} alt='imagem conteúdo'/>
+
+       </div>
+
+        {/*FIMbody*/}
+
     </div>
   )
 }
